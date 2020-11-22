cdd7ea121f9d1c481df765ddbd7315cefed07edf
Joseph Samuels <josephsamuels@Josephs-MacBook-Pro.local>
2020-11-22 15:44:13 -0500

    main - Cleanup.

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import GameLoop from './classes/GameLoop';
-import './styles/styles.css';
-
-window.addEventListener("load", () => {
-  new GameLoop(document.getElementById('root') as HTMLCanvasElement);
-})
